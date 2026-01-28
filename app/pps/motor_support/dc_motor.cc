1a612ca5d0d5f4b79f756002de1fa9b900223169
Bex Saw <bex.bcc171@gmail.com>
2026-01-27 19:31:32 -0800

    clang format

--- app/pps/motor_support/dc_motor.cc
+++ app/pps/motor_support/dc_motor.cc
@@ -1,81 +1,104 @@
 #include "dc_motor.h"
-#include <cstdlib>
 #include <algorithm>
+#include <cstdlib>
 
+namespace LBR
+{
 
-namespace LBR {
-
-Motor::Motor(Drv8245& drv, Encoder& encoder)
-    : _drv(drv), _encoder(encoder) {}
-
-Motor::~Motor() {}
-
-bool Motor::init() {
-	// Initialize driver
-	_drv.init();
-	_initialized = true;
-	return true;
+Motor::Motor(Drv8245& drv, Encoder& encoder) : _drv(drv), _encoder(encoder)
+{
 }
 
-void Motor::motorEnable(bool enable) {
-	if (enable) {
-		_drv.setSleep(false);  // Wake up driver (enable)
-	} else {
-		_drv.enableCoast();    // Set driver to Hi-Z (disable)
-	}
+Motor::~Motor()
+{
 }
 
-void Motor::motorSpeed(int speed) {
-	// Clamp speed to -100 to 100
-	speed = std::clamp(std::abs(speed), 0, 100);
-	_drv.setSpeed(static_cast<uint16_t>(speed));
+bool Motor::init()
+{
+    // Initialize driver
+    _drv.init();
+    _initialized = true;
+    return true;
 }
 
-void Motor::motorDirection(bool forward) {
-    _drv.setDirection(forward ? Drv8245::Direction::Forward : Drv8245::Direction::Reverse);
+void Motor::motorEnable(bool enable)
+{
+    if (enable)
+    {
+        _drv.setSleep(false);  // Wake up driver (enable)
+    }
+    else
+    {
+        _drv.enableCoast();  // Set driver to Hi-Z (disable)
+    }
 }
 
-void Motor::moveDegrees(int degrees, int speed) {
-	// Encoder
-	int initial_ticks = _encoder.getTicks();
-	int target_ticks = initial_ticks + degrees; // Assuming 1 degree = 1 tick for
-		
-		// Move motor to target degrees using encoder feedback
-		bool forward = (degrees >= 0);
-		motorDirection(forward);
-		motorEnable(true);
-		motorSpeed(std::abs(speed));
-		
-		// Simple polling loop (blocking)
-		if (forward) {
-			while (_encoder.getTicks() < target_ticks) {
-				LBR::Utils::DelayMs(1); 
-			}
-		} else {
-			while (_encoder.getTicks() > target_ticks) {
-				LBR::Utils::DelayMs(1); 
-			}
-		}
-		motorSpeed(0);
-		motorEnable(false);
+void Motor::motorSpeed(int speed)
+{
+    // Clamp speed to -100 to 100
+    speed = std::clamp(std::abs(speed), 0, 100);
+    _drv.setSpeed(static_cast<uint16_t>(speed));
 }
 
-int Motor::getTicks() const {
-	return _encoder.getTicks();
+void Motor::motorDirection(bool forward)
+{
+    _drv.setDirection(forward ? Drv8245::Direction::Forward
+                              : Drv8245::Direction::Reverse);
 }
 
-int Motor::getStatus() const {
-	// Return status code (0 for OK, nonzero for error)
-	if( !_initialized) {
-		return -1; // Not initialized
-	}
-	if (_drv.checkFault()) {
-		return -2; // Driver fault
-	}
-	if (_encoder.getStatus() != 0) {
-		return -3; // Encoder error
-	} 
-	return 0;
+void Motor::moveDegrees(int degrees, int speed)
+{
+    // Encoder
+    int initial_ticks = _encoder.getTicks();
+    int target_ticks =
+        initial_ticks + degrees;  // Assuming 1 degree = 1 tick for
+
+    // Move motor to target degrees using encoder feedback
+    bool forward = (degrees >= 0);
+    motorDirection(forward);
+    motorEnable(true);
+    motorSpeed(std::abs(speed));
+
+    // Simple polling loop (blocking)
+    if (forward)
+    {
+        while (_encoder.getTicks() < target_ticks)
+        {
+            LBR::Utils::DelayMs(1);
+        }
+    }
+    else
+    {
+        while (_encoder.getTicks() > target_ticks)
+        {
+            LBR::Utils::DelayMs(1);
+        }
+    }
+    motorSpeed(0);
+    motorEnable(false);
 }
 
-} // namespace LBR
+int Motor::getTicks() const
+{
+    return _encoder.getTicks();
+}
+
+int Motor::getStatus() const
+{
+    // Return status code (0 for OK, nonzero for error)
+    if (!_initialized)
+    {
+        return -1;  // Not initialized
+    }
+    if (_drv.checkFault())
+    {
+        return -2;  // Driver fault
+    }
+    if (_encoder.getStatus() != 0)
+    {
+        return -3;  // Encoder error
+    }
+    return 0;
+}
+
+}  // namespace LBR
--- common/drivers/platform/stm32l4/reg_helpers.h
+++ common/drivers/platform/stm32l4/reg_helpers.h
@@ -5,30 +5,31 @@
 
 // Example: Set a bit
 #ifndef SET_BIT
-#define SET_BIT(REG, BIT)     ((REG) |= (BIT))
+#define SET_BIT(REG, BIT) ((REG) |= (BIT))
 #endif
 
 // Example: Clear a bit
 #ifndef CLEAR_BIT
-#define CLEAR_BIT(REG, BIT)   ((REG) &= ~(BIT))
+#define CLEAR_BIT(REG, BIT) ((REG) &= ~(BIT))
 #endif
 
 // Example: Read a bit
 #ifndef READ_BIT
-#define READ_BIT(REG, BIT)    ((REG) & (BIT))
+#define READ_BIT(REG, BIT) ((REG) & (BIT))
 #endif
 
 // Example: Write a value to a register
 #ifndef WRITE_REG
-#define WRITE_REG(REG, VAL)   ((REG) = (VAL))
+#define WRITE_REG(REG, VAL) ((REG) = (VAL))
 #endif
 
 // Example: Modify a register
 #ifndef MODIFY_REG
-#define MODIFY_REG(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_BIT((REG), ~(CLEARMASK))) | (SETMASK))))
+#define MODIFY_REG(REG, CLEARMASK, SETMASK) \
+    WRITE_REG((REG), (((READ_BIT((REG), ~(CLEARMASK))) | (SETMASK))))
 #endif
 
 // Example: Set a register field (bitfield)
 #ifndef SET_REG
-#define SET_REG(REG, MASK, VAL)  ((REG) = ((REG) & ~(MASK)) | ((VAL) & (MASK)))
+#define SET_REG(REG, MASK, VAL) ((REG) = ((REG) & ~(MASK)) | ((VAL) & (MASK)))
 #endif
